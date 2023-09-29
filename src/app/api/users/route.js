f6df26d5a23e1b2433a381a401ed977d9fe66340
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2023-09-29 09:25:45 +0300

    added sharp library

--- src/app/api/users/route.js
+++
@@ -1,7 +0,0 @@
-import dbConnect from "@/DB/connect";
-import { NextResponse } from "next/server";
-import User from "@/DB/Models/Teacher";
-
-export async function GET(req) {
-  return NextResponse.json({ success: "done" });
-}
